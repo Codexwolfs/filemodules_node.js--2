f9e81c8667682e4390da0b2da6855b04bc46457d
rajatpayal002 <rajatpayal@outlook.in>
2021-06-22 16:23:32 +0530

    server not working

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const http = require('http');
+//last check -
+//const hostname = 'localhost';
+const fs = require('fs');
 const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer((req,res)=>{
@@ -8,12 +11,25 @@ const server = http.createServer((req,res)=>{
   // console.log('request',req.method)
   //request object in node js 
 
-res.setHeader('contect-type','text/plan');
-res.write('welcome to rajat site ');
+// res.setHeader('contect-type','text/plan');
+res.setHeader('contect-type','text/html');
+fs.readFile('./server/view/index.html',(err,data)=>{
+  if(err){ //if else loop 
+    console.log(err);
+    res.end();
+  
+  }else{
+    res.write(data);
+    res.end();
+  }
+})
+// res.write('<body class="app"></body>');
+// res.write('<h3> i am rajat</h3>');
+// res.write('<p>welcome to rajat site</p>');
 res.end(); 
   
 
-});
+});  
 
 server.listen(port,()=>{
     console.log(`listening on port ${port}`)
